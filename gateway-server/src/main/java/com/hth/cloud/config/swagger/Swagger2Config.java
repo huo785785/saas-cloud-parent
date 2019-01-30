16a755cd577051718c63dd61d9c165da509fd3a2
huoth <1064712776@qq.com>
2019-01-30 10:16:45 +0800

    去掉无用注解

--- gateway-server/src/main/java/com/hth/cloud/config/swagger/Swagger2Config.java
+++ gateway-server/src/main/java/com/hth/cloud/config/swagger/Swagger2Config.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
